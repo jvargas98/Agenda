cd093db2d50778dbaed0ee75d36ac1ea7d2ed26d
Jorge Alejandro Vargas Martinez <36525675+jvargas98@users.noreply.github.com>
2018-04-17 11:11:05 -0500

    Primer Commit
    primero

---
+++ Agenda/Agenda.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class Agenda
+    {
+        Contacto[] personas = new Contacto[15];
+        private int i = 0;
+        public Agenda()
+        {
+
+        }
+        public void Agregar(Contacto contacto)
+        {
+            personas[i] = contacto;
+            i++;
+        }
+
+        public string Buscar(int telefono)
+        {
+            string contacto = "";
+            int contador = 0;
+            while (contador < personas.Length - 1 && personas[contador].Telefono != telefono)
+            {
+                contador++;
+            }
+            if (contador > personas.Length-1)
+            {
+            }
+            else
+            {
+                return contacto = personas[contador].ToString();
+            }
+            return null;
+        }
+
+        public void Eliminar(int telefono)
+        {
+            int contador1 = 0;
+            while (contador1 < personas.Length-1 && personas[contador1].Telefono != telefono)
+            {
+                contador1++;
+            }
+            if (contador1 > personas.Length-1)
+            {
+            }
+            else
+            {
+                for (int contador2 = contador1; contador2 < personas.Length-2; contador2++)
+                {
+                    personas[contador2] = personas[contador2 + 1];
+                }
+                i--;
+            }
+        }
+        public void Insertar(int posicion, Contacto contacto)
+        {
+           
+            for (int contador = personas.Length-2; contador >= posicion; contador--)
+            {
+               personas[contador+1] = personas[contador];
+            }
+            personas[posicion] = contacto;
+            i++;
+        }
+        public override string ToString()
+        {
+            string cadena = "";
+            int contador = 0;
+            while(personas[contador] != null)
+            {
+                cadena += personas[contador].ToString();
+                contador++;
+            }
+            return cadena;
+        }
+    }
+}
---
+++ Agenda/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+
+        Agenda agenda = new Agenda();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int telefono = int.Parse(txtTelefono.Text);
+            string nombre = txtNombre.Text;
+            string apellidoPaterno = txtApellidoPaterno.Text;
+            string apellidoMaterno = txtApellidoMaterno.Text;
+            int edad = int.Parse(txtEdad.Text);
+            string correo = txtCorreo.Text;
+
+            Contacto contacto = new Contacto(telefono,nombre,apellidoPaterno,apellidoMaterno,edad,correo);
+            agenda.Agregar(contacto);
+
+            txtTelefono.Clear();
+            txtNombre.Clear();
+            txtApellidoPaterno.Clear();
+            txtApellidoMaterno.Clear();
+            txtEdad.Clear();
+            txtCorreo.Clear();
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            txtListado.Text = agenda.ToString();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string[] informacion = new string[6];
+            string contacto = "";
+            int telefono = int.Parse(txtTelefono.Text);
+            contacto = agenda.Buscar(telefono);
+            informacion = contacto.Split(' ');
+
+            txtTelefono.Text = informacion[0];
+            txtNombre.Text = informacion[1];
+            txtApellidoPaterno.Text = informacion[2];
+            txtApellidoMaterno.Text = informacion[3];
+            txtEdad.Text = informacion[4];
+            txtCorreo.Text = informacion[5];
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int telefono = int.Parse(txtTelefono.Text);
+
+            agenda.Eliminar(telefono);
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            int posicion = int.Parse(txtPosicion.Text);
+            int telefono = int.Parse(txtTelefono.Text);
+            string nombre = txtNombre.Text;
+            string apellidoPaterno = txtApellidoPaterno.Text;
+            string apellidoMaterno = txtApellidoMaterno.Text;
+            int edad = int.Parse(txtEdad.Text);
+            string correo = txtCorreo.Text;
+
+            Contacto contacto = new Contacto(telefono, nombre, apellidoPaterno, apellidoMaterno, edad, correo);
+
+            agenda.Insertar(posicion, contacto);
+
+            txtTelefono.Clear();
+            txtNombre.Clear();
+            txtApellidoPaterno.Clear();
+            txtApellidoMaterno.Clear();
+            txtEdad.Clear();
+            txtCorreo.Clear();
+           
+        }
+    }
+}
